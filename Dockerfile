e4e50c5a77b52e04bbe98fb8a3ff7aadeb46d878
Grzegorz Lehmann <grzegorz.lehmann@synvia.de>
2022-10-27 23:44:48 +0200

    Add WebDAV configuration to entry script
    Rename the script to better reflect its extended role

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM alpine:latest
 
 LABEL Description="FTP-WebDAV bridge Docker container based on davfs2, vsftpd and alpine"
 
-RUN apk --no-cache add vsftpd
+# install vsftpd for ftp and davfs2 WebDAV client
+RUN apk --no-cache add vsftpd davfs2
 
+# FTP configuration
 ENV FTP_USER=username
 ENV FTP_PASS=password
 ENV PASV_ENABLE=YES
@@ -14,10 +16,20 @@ ENV ANON_ENABLE=NO
 ENV NO_ANON_PASSWD=NO
 ENV ANON_ROOT=/var/ftp
 
-COPY vsftpd.sh /usr/sbin/
+# configure vsftp
+COPY ftp-dav-bridge.sh /usr/sbin/
+RUN chmod +x /usr/sbin/ftp-dav-bridge.sh
 
-RUN chmod +x /usr/sbin/vsftpd.sh
-
+# expose ftp ports, PASSV ports are optional
 EXPOSE 20 21
 
-ENTRYPOINT ["/usr/sbin/vsftpd.sh"]
+# WebDAV configuration
+ENV WEBDAV_DIR_NAME=webdav
+# make sure to change those
+ENV WEBDAV_USER=${FTP_USER}
+ENV WEBDAV_PASS=${FTP_PASS}
+ENV WEBDAV_URL=https://change_me
+# disable upload delay for immediate webdav upload (https://www.systutorials.com/docs/linux/man/5-davfs2.conf/)
+ENV WEBDAV_DELAY_UPLOAD=0
+
+ENTRYPOINT ["/usr/sbin/ftp-dav-bridge.sh"]
---
+++ ftp-dav-bridge.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Create user and add it to the 'ftp' group
+adduser -G ftp -s /bin/sh -D $FTP_USER
+
+# Set password of the user
+echo "$FTP_USER:$FTP_PASS" | chpasswd
+
+# Add config lines to vsftpd.conf
+echo "# Lines added
+seccomp_sandbox=NO
+local_enable=YES
+write_enable=YES
+chroot_local_user=YES
+passwd_chroot_enable=YES
+allow_writeable_chroot=YES
+
+ftpd_banner=Welcome to vsftpd
+max_clients=10
+max_per_ip=5
+local_umask=022
+
+pasv_enable=$PASV_ENABLE
+pasv_max_port=$PASV_MAX_PORT
+pasv_min_port=$PASV_MIN_PORT
+pasv_address=$PASV_ADDRESS
+
+anonymous_enable=$ANON_ENABLE
+no_anon_password=$NO_ANON_PASSWD
+anon_root=$ANON_ROOT
+" >>/etc/vsftpd/vsftpd.conf
+
+# create directory for webdav mount and give user access to it
+echo "Creating directory for WebDAV mount '/home/$FTP_USER/$WEBDAV_DIR_NAME'"
+mkdir /home/$FTP_USER/$WEBDAV_DIR_NAME
+chown $FTP_USER /home/$FTP_USER/$WEBDAV_DIR_NAME
+chmod 770 /home/$FTP_USER/$WEBDAV_DIR_NAME
+
+# configure davfs2 WebDAV mount secret
+echo "/home/$FTP_USER/$WEBDAV_DIR_NAME $WEBDAV_USER $WEBDAV_PASS" >/etc/davfs2/secrets
+chown root /etc/davfs2/secrets
+chmod 600 /etc/davfs2/secrets
+
+# Disable upload delay
+echo "Configuring WebDAV upload delay of ${WEBDAV_DELAY_UPLOAD}s"
+echo "delay_upload $WEBDAV_DELAY_UPLOAD" >/etc/davfs2/davfs2.conf
+
+# create webdav mount for nextcloud
+echo "Mounting $WEBDAV_URL into /home/$FTP_USER/$WEBDAV_DIR_NAME"
+# sudo mount -t davfs -o noexec $WEBDAV_URL /home/$FTP_USER/$WEBDAV_DIR_NAME
+echo "$WEBDAV_URL /home/$FTP_USER/$WEBDAV_DIR_NAME davfs user,file_mode=600,dir_mode=700 0 1" >/etc/fstab
+
+# start vsftpd
+echo "Starting FTP server"
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
